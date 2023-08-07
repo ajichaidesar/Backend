eb77ffdf17a835324fada0d6661ba2c3553322bc
aji chaidesar <ajichaidesar@gmail.com>
2023-08-07 17:13:26 +0700

    update integrasi question

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { Kontak } from './kontak/entities/kontak.entity';
 import { KontakModule } from './kontak/kontak.module';
 import { Layanan } from './layanan/entities/layanan.entity';
 import { LayananModule } from './layanan/layanan.module';
-import { Pesans } from './pesan/entities/pesan.entity';
+import { Pesan } from './pesan/entities/pesan.entity';
 import { PesanModule } from './pesan/pesan.module';
 import { Program } from './program/entities/program.entity';
 import { ProgramsModule } from './program/program.module';
@@ -29,7 +29,7 @@ import { RegistrasiModule } from './registrasi/registrasi.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PW,
       database: process.env.DB_NAME,
-      entities: [ Artikels, Galeries, Infos, Kontak, Layanan, Pesans, Program, Question, Registrasis,], 
+      entities: [ Artikels, Galeries, Infos, Kontak, Layanan, Pesan, Program, Question, Registrasis,], 
       synchronize: false, 
     }),
     ArtikelModule,
